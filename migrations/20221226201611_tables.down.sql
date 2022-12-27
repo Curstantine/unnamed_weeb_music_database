4cc727bd0ef614e3febcfea8b1539fd113fbd247
David Alasow <davidalasow@gmail.com>
2022-12-27 01:24:27 +0100

    Add missing down migration for tables.

--- migrations/20221226201611_tables.down.sql
+++ migrations/20221226201611_tables.down.sql
@@ -1,1 +1,6 @@
--- Add down migration script here
+/* Drop the tables created in the up migration */
+DROP TABLE IF EXISTS songs;
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS releases;
+DROP TABLE IF EXISTS songs_artists;
+DROP TABLE IF EXISTS songs_releases;
